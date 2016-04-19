3c6f5032a217e1da2aa164aa71c9c4f71e908c2c
HolyLiu <holyliu211@gmail.com>
2016-04-19 09:14:24 -0400

    Added a plugin folder.

---
+++ openvision3/plugin/openvision.h
@@ -0,0 +1,13 @@
+//
+//  openvision.h
+//  openvision3
+//
+//  Created by HONG on 2016-04-19.
+//  Copyright © 2016 ZHIMAABC. All rights reserved.
+//
+
+#ifndef openvision_h
+#define openvision_h
+
+
+#endif /* openvision_h */
