4fa6ff8feb4cbbeeb0c0e8be3564086245fc307d
HolyLiu <holyliu211@gmail.com>
2016-04-23 22:05:22 -0400

    Added a view to show original video.

--- openvision3/ViewController.mm
+++ openvision3/ViewController.mm
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *vImagePreview;
+@property (weak, nonatomic) IBOutlet UIImageView *vImageOutput;
 
 @end
 
@@ -19,11 +20,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.videoCamera = [[CvVideoCamera alloc] initWithParentView:self.vImagePreview];
+    self.videoCamera = [[CvVideoCamera alloc] initWithParentView:self.vImageOutput];
     self.videoCamera.delegate = self;
     self.videoCamera.defaultAVCaptureDevicePosition = AVCaptureDevicePositionFront;
-    self.videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationLandscapeRight;
-    self.videoCamera.defaultAVCaptureSessionPreset = AVCaptureSessionPresetHigh;
+    self.videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationPortrait;
+    self.videoCamera.defaultAVCaptureSessionPreset = AVCaptureSessionPreset352x288;
     self.videoCamera.defaultFPS = 15;
     
 }
@@ -31,6 +32,11 @@
 - (void)viewDidAppear:(BOOL)animated
 {
     [self.videoCamera start];
+    
+    AVCaptureVideoPreviewLayer *leftPreviewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.videoCamera.captureSession];
+    leftPreviewLayer.frame = self.vImagePreview.bounds; // Assume you want the preview layer to fill the view.
+    leftPreviewLayer.videoGravity = AVLayerVideoGravityResize;
+    [self.vImagePreview.layer addSublayer:leftPreviewLayer];
 }
 
 - (void)viewDidDisappear:(BOOL)animated
@@ -41,6 +47,12 @@
 #pragma mark Button Action
 - (IBAction)switchCamera:(id)sender {
     [self.videoCamera switchCameras];
+    
+    self.vImagePreview.layer.sublayers = nil;
+    AVCaptureVideoPreviewLayer *leftPreviewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.videoCamera.captureSession];
+    leftPreviewLayer.frame = self.vImagePreview.bounds; // Assume you want the preview layer to fill the view.
+    leftPreviewLayer.videoGravity = AVLayerVideoGravityResize;
+    [self.vImagePreview.layer addSublayer:leftPreviewLayer];
 }
 
 #pragma mark - Protocol CvVideoCameraDelegate
@@ -48,13 +60,16 @@
 #ifdef __cplusplus
 - (void)processImage:(cv::Mat&)image;
 {
+    cv::Size strel_size;
+    strel_size = image.size();
+    char str[200];
+    sprintf(str,"%d x %d IttiSaliency",strel_size.width, strel_size.height);
     cv::Mat outputMat;
     SaliencyDetection(image,outputMat);
     image = outputMat;
     
-    cv::putText(image, "Hello, please add your opencv code at there!", cvPoint(30,30),
-                cv::FONT_HERSHEY_SIMPLEX, 0.8, cvScalar(255,0,0), 1, CV_AA,false);
-    
+    cv::putText(image, str, cvPoint(10,20),
+                cv::FONT_HERSHEY_PLAIN, 0.8, cvScalar(255,0,0), 1, CV_AA,false);
 }
 #endif
 
